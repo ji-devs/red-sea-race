5fd800dc69173dcbb14cae202c6b4ab25732bcfe
David Komer <david.komer@gmail.com>
2020-04-05 22:32:38 +0300

    cycling bg sprites

--- src/systems.rs
+++ src/systems.rs
@@ -115,24 +115,37 @@ pub fn run(
 #[system(TrashSys)]
 pub fn run(
     mut all_storages: &mut AllStorages,
-    translations: &Translation,
-    sprites: &Sprite,
-    renderables: &Renderable,
 ) {
-    let off_screen:Vec<EntityId> = (&translations, &sprites, &renderables)
-        .iter()
-        .with_id()
-        .filter(|(_, (pos, layer, renderable))| {
-            let right_bound = pos.x + (renderable.texture.tex_width as f64); 
+    
+    let entities_to_delete:Vec<EntityId> = {
+        let (translations, sprites, renderables) = all_storages.borrow::<(&Translation, &Sprite, &Renderable)>();
 
-            //less than 0.0 is probably fine, but let's give it a bit of padding to be safe
-            if right_bound < 2.0 { true } else { false }
-        })
-        .map(|(entity, _)| entity)
-        .collect();
+        let entities_to_delete:Vec<EntityId> = 
+            (&translations, &sprites, &renderables)
+                .iter()
+                .with_id()
+                .filter(|(_, (pos, layer, renderable))| {
+                    let right_bound = pos.x + (renderable.texture.tex_width as f64); 
 
-    for entity in off_screen {
-        log::info!("Delete {:?}", entity);
+                    //less than 0.0 is probably fine, but let's give it a bit of padding to be safe
+                    if right_bound < 2.0 { true } else { false }
+                })
+                .map(|(entity, _)| entity)
+                .collect();
+       
+        
+        let mut hierarchy_storages  = all_storages.borrow::<(EntitiesMut, &mut Parent, &mut Child)>();
+
+        let mut hierarchy_storages = (&mut hierarchy_storages.0, &mut hierarchy_storages.1, &mut hierarchy_storages.2);
+        for entity in entities_to_delete.iter() {
+            hierarchy_storages.remove_single(*entity);
+        }
+
+        entities_to_delete
+    };
+
+    for entity in entities_to_delete {
+        all_storages.delete(entity);
     }
     
 }
