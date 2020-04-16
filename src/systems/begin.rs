6c1968ccda4f71a71b1c3f7d025fa33fc4e70157
David Komer <david.komer@gmail.com>
2020-04-16 21:13:56 +0300

    controller input

--- src/systems/begin.rs
+++
@@ -1,6 +0,0 @@
-use shipyard::prelude::*;
-use crate::tick::TickBegin;
-
-#[system(TickBeginSys)]
-pub fn run(_tick: Unique<&TickBegin>) {
-}
--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -1,16 +1,16 @@
-mod begin;
 mod bg;
 mod draw;
 mod end;
 mod motion;
 mod remove;
 mod tweens;
+mod controller;
 
 use shipyard_scenegraph::{self as sg};
 use shipyard::prelude::*;
 
-use begin::*;
 use bg::*;
+use controller::*;
 use draw::*;
 use end::*;
 use motion::*;
@@ -25,10 +25,10 @@ pub const TWEENS: &str = "TWEENS";
 pub const TRANSFORMS: &str = "TRANSFORMS";
 
 pub fn register_workloads(world: &World) {
-    world.add_workload::<TickBeginSys, _>(TICK_BEGIN);
+    world.add_workload::<ControllerStartSys, _>(TICK_BEGIN);
     world.add_workload::<(TweenEventSys, TweenUpdateSys, TweenFinishSys), _>(TWEENS);
     world.add_workload::<(MotionSys, BgCycleSys, BgSpawnSys, TrashSys), _>(TICK_UPDATE);
     world.add_workload::<(sg::systems::TrsToLocal, sg::systems::LocalToWorld), _>(TRANSFORMS);
     world.add_workload::<TickDrawSys, _>(TICK_DRAW);
-    world.add_workload::<TickEndSys, _>(TICK_END);
+    world.add_workload::<(TickEndSys, ControllerEndSys), _>(TICK_END);
 }
